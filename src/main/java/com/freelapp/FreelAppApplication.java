b764627dcabd31cdcf16ad210904fe60978ef422
Antonietta Brenga <antobrg90@hotmail.it>
2024-10-16 16:00:11 +0200

    creazione del progetto Spring Boot

---
+++ src/main/java/com/freelapp/FreelAppApplication.java
@@ -0,0 +1,13 @@
+package com.freelapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FreelAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FreelAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/freelapp/FreelAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.freelapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FreelAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
