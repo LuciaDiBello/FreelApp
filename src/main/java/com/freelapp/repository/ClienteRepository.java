44aa79c0796aa37669174edebb5ab7c5c31155cf
FrancescoDAmbrosio1 <158843577+FrancescoDAmbrosio1@users.noreply.github.com>
2024-10-25 00:16:57 +0200

    ultimata ricerca per partita iva

--- src/main/java/com/freelapp/repository/ClienteRepository.java
+++ src/main/java/com/freelapp/repository/ClienteRepository.java
@@ -3,10 +3,17 @@ package com.freelapp.repository;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import com.freelapp.model.Cliente;
 
 public interface ClienteRepository extends JpaRepository<Cliente, Integer>{
 	
-    List<Cliente> findByPartitaIva(String partitaIva);
+//    List<Cliente> findByPartitaIva(String partitaIva);
+    
+    
+//    query custom per la ricerca per partita IVA
+    @Query("SELECT c FROM Cliente c WHERE c.partitaIva LIKE '%'||:input||'%' ")
+	
+    public List<Cliente> search( String input);
 }
