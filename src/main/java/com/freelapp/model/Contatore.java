5ea3956cde4740d1104c7b785ed7471b7eb5ca39
RosaCielo <ashyzan@gmail.com>
2024-10-30 10:26:30 +0100

    Added getter e setters to contatore
    and added a record stop_numbers to write how many time contatore is stopped (for statistics only)

--- src/main/java/com/freelapp/model/Contatore.java
+++ src/main/java/com/freelapp/model/Contatore.java
@@ -23,15 +23,62 @@ public class Contatore {
 	@NotNull(message = "Start non può essere null")
 	private Time start;
     
-    @Column(name = "pause", nullable = false)
-	@NotNull(message = "Stop non può essere null")
+    @Column(name = "pause", nullable = true)
 	private Time pause;
 	
-    @Column(name = "stop", nullable = false)
-	@NotNull(message = "Stop non può essere null")
+    @Column(name = "stop", nullable = true)
 	private Time stop;
+    
+    @Column(name = "stop_numbers", nullable = true)
+    	private int stop_numbers;
 	
     @OneToOne(mappedBy = "contatore")
     private Task task;
+    
+    // getter e setters
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public Time getStart() {
+        return start;
+    }
+
+    public void setStart(Time start) {
+        this.start = start;
+    }
+
+    public Time getPause() {
+        return pause;
+    }
+
+    public void setPause(Time pause) {
+        this.pause = pause;
+    }
+
+    public Time getStop() {
+        return stop;
+    }
+
+    public void setStop(Time stop) {
+        this.stop = stop;
+    }
+
+    public Task getTask() {
+        return task;
+    }
+
+    public void setTask(Task task) {
+        this.task = task;
+    }
+    
+
+    
+    
 	
 }
